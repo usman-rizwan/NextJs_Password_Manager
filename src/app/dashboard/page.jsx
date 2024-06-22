00109afe281c2ea99502f6ee69e47f38b2541a31
usman_rizwan <usmanrizwan771@gmail.com>
2024-06-22 13:46:57 +0500

    Update page.jsx

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -16,11 +16,13 @@ const DashboardPageContent  = () => {
 
   const retrieveDataFromDB = async () => {
     try {
+      console.log("Requsest for data ==>");
       const response = await axios.get("/api/data/getData", {
         params: { id: searchParamsId },
       });
       // console.log("response===>", response);
       if (response.status === 200 && response.data.data.length > 0) {
+        console.log("data recieved");
         setFormData(response.data.data);
       } else if (response.data.data.status === 404) {
         toast.error("No data found");
@@ -35,6 +37,7 @@ const DashboardPageContent  = () => {
 
   useEffect(() => {
     if (searchParamsId) {
+      console.log("Get data ===>");
       retrieveDataFromDB();
     }
   }, [searchParamsId]);
